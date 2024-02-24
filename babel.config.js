841b236391b42063cdee5472a4c044ef243258d7
Miguel Lo-A-Foe <miguel@loafoe.dev>
2024-02-24 15:38:53 +0100

    add configs for jest, babel, ts to work with esm and no longer ignore test files in tsc

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,13 @@
-module.exports = {
-  presets: [
-    ["@babel/preset-env", { targets: { node: "current" } }],
-    "@babel/preset-typescript",
-  ],
-};
+/** @type {import('@babel/core').ConfigFunction} */
+function config(api) {
+  api.cache(true);
+
+  return {
+    presets: [
+      ["@babel/preset-env", { targets: { node: "current" } }],
+      "@babel/preset-typescript",
+    ],
+  };
+}
+
+export default config;
---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/** @type {import('jest').Config} */
+const config = {
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+};
+
+export default config;
